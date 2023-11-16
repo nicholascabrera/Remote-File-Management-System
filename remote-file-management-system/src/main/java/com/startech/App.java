b0342e96b3cfc25ea8a9a1c8bfde7482141edbc1
Nicholas Cabrera <ncabrera528@gmail.com>
2023-11-16 14:47:14 -0500

    first commit

---
+++ remote-file-management-system/src/main/java/com/startech/App.java
@@ -0,0 +1,13 @@
+package com.startech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
